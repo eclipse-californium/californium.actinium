9c3481c1bad24df975e35af2470bc42ccabf87f1
Yassin N. Hassan <hassany@student.ethz.ch>
2015-10-25 17:44:34 +0900

    Test Abort

--- run/appserver/installed/request_test.js
+++ run/appserver/installed/request_test.js
@@ -2,6 +2,7 @@ var uri = 'coap://localhost/'; // set via POST
 var method = 'GET'; // set via POST
 var result = '0';
 var async = true;
+var abort = false;
 app.dump('Set RTT URI via POST');
 
 function pollNode() {
@@ -16,26 +17,27 @@ function pollNode() {
         result = 'TIMEOUT';
     };
     client.send('');
+    if(abort){
+        client.abort();
+    }
 }
 
 app.root.onget = function(request) {
-    request.accept();
-
-    dump('RTT waiting...');
-
     request.respond(ResponseCode.CONTENT, result);
 }
 
 app.root.onpost = function(request) {
     var value = request.requestText.split(' ');
-    if (value.length != 3) {
+    if (value.length != 3 && value.length != 4) {
         request.respond(128);
         return
     }
     uri = value[1];
     method = value[0];
     async = value[2]=='true';
+    abort = value[3]=='true';
     app.dump('RTT URI: ' + uri);
+    app.dump('abort: ' + abort);
     pollNode();
     app.dump('Done');
     request.respond(ResponseCode.CHANGED);
--- src/test/java/org/eclipse/californium/actinium/RequestTest.java
+++ src/test/java/org/eclipse/californium/actinium/RequestTest.java
@@ -51,15 +51,15 @@ public class RequestTest extends BaseServerTest {
 		if(async) {
 			Thread.sleep(1000);
 		}
-		assertEquals(CoAP.ResponseCode.CHANGED, configureRTT.waitForResponse(TIMEOUT*30).getCode());
+		assertEquals(CoAP.ResponseCode.CHANGED, configureRTT.waitForResponse(TIMEOUT * 30).getCode());
 		Request runRTT = Request.newGet();
 		runRTT.setURI(baseURL + "apps/running/requ");
 		runRTT.send();
-		Response result = runRTT.waitForResponse(TIMEOUT*10000);
+		Response result = runRTT.waitForResponse(TIMEOUT * 10000);
 		assertEquals(CoAP.ResponseCode.CONTENT, result.getCode());
 		assertEquals("OK", result.getPayloadString());
 		Request checkCounter = Request.newGet();
-		checkCounter.setURI(baseURL+"apps/running/counter");
+		checkCounter.setURI(baseURL + "apps/running/counter");
 		checkCounter.send();
 		Response counterResult = checkCounter.waitForResponse(TIMEOUT);
 		assertEquals(CoAP.ResponseCode.CONTENT, counterResult.getCode());
@@ -91,6 +91,35 @@ public class RequestTest extends BaseServerTest {
 		Response result = runRTT.waitForResponse(TIMEOUT);
 		assertEquals(CoAP.ResponseCode.CONTENT, result.getCode());
 		assertEquals("TIMEOUT", result.getPayloadString());
+
+	}
+
+
+	@Test
+	public void testAbort() throws InterruptedException, FileNotFoundException {
+		if(!async)
+			return;
+		//Install Request
+		installScript("requestTest", new File("run/appserver/installed/request_test.js"));
+		createInstance("requestTest", "requ");
+		testCheckIfInstanceExists("requ");
+		testCheckInstance("requestTest", "requ");
+		Thread.sleep(3000);
+		testCheckIfInstanceIsRunning("requ");
+		Request configureRTT = Request.newPost();
+		configureRTT.setURI(baseURL + "apps/running/requ");
+		configureRTT.setPayload("POST coap://localhost:2222 true true");
+		configureRTT.send();
+		if(async) {
+			Thread.sleep(3000);
+		}
+		assertEquals(CoAP.ResponseCode.CHANGED, configureRTT.waitForResponse(TIMEOUT*30).getCode());
+		Request runRTT = Request.newGet();
+		runRTT.setURI(baseURL + "apps/running/requ");
+		runRTT.send();
+		Response result = runRTT.waitForResponse(TIMEOUT);
+		assertEquals(CoAP.ResponseCode.CONTENT, result.getCode());
+		assertEquals("0", result.getPayloadString());
 
 	}
 
