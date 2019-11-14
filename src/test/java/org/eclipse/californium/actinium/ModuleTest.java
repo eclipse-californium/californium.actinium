9fd99a1a3a0bfdc1f45c5854c6a98295ffd1180d
Achim Kraus <achim.kraus@bosch-si.com>
2019-11-14 18:35:26 +0100

    Add license header.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- src/test/java/org/eclipse/californium/actinium/ModuleTest.java
+++ src/test/java/org/eclipse/californium/actinium/ModuleTest.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Institute for Pervasive Computing, ETH Zurich and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Yassin N. Hassan - initial implementation
+ *    Matthias Kovatsch - creator and main architect
+ ******************************************************************************/
 package org.eclipse.californium.actinium;
 
 import org.eclipse.californium.core.coap.CoAP;
